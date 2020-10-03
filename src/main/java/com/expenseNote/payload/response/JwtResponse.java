1e37d7be81a25530241cb2759ae1818e7296a07f
Victor Ihedioha <ihedioha.victor@gmail.com>
2020-10-03 06:31:14 +0100

    Added JWT responses

--- src/main/java/com/expenseNote/payload/response/JwtResponse.java
+++ src/main/java/com/expenseNote/payload/response/JwtResponse.java
@@ -1,4 +1,85 @@
 package com.expenseNote.payload.response;
 
+import java.util.List;
+
 public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public JwtResponse(String token, Long id, String firstName, String lastName, String username, String email, List<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
 }
