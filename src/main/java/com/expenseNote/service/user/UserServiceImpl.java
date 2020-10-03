6de071d279c5b23b1124a393a6085416d87031b1
Victor Ihedioha <ihedioha.victor@gmail.com>
2020-10-03 05:06:26 +0100

    Implemented User ServiceDetails From Spring Security

--- src/main/java/com/expenseNote/service/user/UserServiceImpl.java
+++ src/main/java/com/expenseNote/service/user/UserServiceImpl.java
@@ -1,4 +1,26 @@
 package com.expenseNote.service.user;
 
-public class UserServiceImpl {
+import com.expenseNote.model.User;
+import com.expenseNote.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with this username: "+ username));
+
+        return UserService.build(user);
+    }
 }
