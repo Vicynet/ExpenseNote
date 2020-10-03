e9c8dc1418066d0a82be525012a34995882f79c9
Victor Ihedioha <ihedioha.victor@gmail.com>
2020-10-03 05:23:17 +0100

    Defined Component Class For JWT Entry Point

--- src/main/java/com/expenseNote/security/WebSecurityConfig.java
+++ src/main/java/com/expenseNote/security/WebSecurityConfig.java
@@ -1,4 +1,21 @@
 package com.expenseNote.security;
 
-public class WebSecurityConfig {
+import com.expenseNote.security.jwt.AuthEntryPointJwt;
+import com.expenseNote.service.user.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserServiceImpl userService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
 }
