2281d2e0dd85d1d04628353d42a651d22c90a78c
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-02-15 00:44:39 +0100

    Tests for transaction service done

---
+++ src/test/java/com/expenseNote/apps/transaction/service/TransactionServiceTest.java
@@ -0,0 +1,89 @@
+package com.expenseNote.apps.transaction.service;
+
+import com.expenseNote.apps.transaction.model.Transaction;
+import com.expenseNote.apps.transaction.repository.TransactionRepository;
+import com.expenseNote.apps.user.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+class TransactionServiceTest {
+
+    @Mock
+    TransactionRepository transactionRepository;
+
+    @InjectMocks
+    TransactionService transactionService;
+
+    Transaction transaction;
+
+    Collection<Transaction> transactions;
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        transactionService = new TransactionServiceImpl();
+        transaction = new Transaction();
+        transactions = new ArrayList<>();
+        user = new User();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void findByUser() {
+        when(transactionService.findByUser(user)).thenReturn(transactions);
+        transactionService.findByUser(user);
+        verify(transactionRepository, times(1)).findByUser(user);
+    }
+
+    @Test
+    void save() {
+        when(transactionService.save(transaction)).thenReturn(transaction);
+        transactionService.save(transaction);
+        verify(transactionRepository, times(1)).save(transaction);
+    }
+
+    @Test
+    void findById() {
+        when(transactionService.findById(transaction.getId())).thenReturn(Optional.of(transaction));
+        transactionService.findById(transaction.getId());
+        verify(transactionRepository, times(1)).findById(transaction.getId());
+    }
+
+    @Test
+    void update() {
+        when(transactionService.update(transaction)).thenReturn(transaction);
+        transactionService.update(transaction);
+        verify(transactionRepository, times(1)).save(transaction);
+    }
+
+    @Test
+    void findByType() {
+        when(transactionService.findByType(transaction.getTransactionType())).thenReturn(Optional.of(transaction));
+        transactionService.findByType(transaction.getTransactionType());
+        verify(transactionRepository, times(1)).findByType(transaction.getTransactionType());
+    }
+
+    @Test
+    void deleteById() {
+        doNothing().when(transactionRepository).deleteById(transaction.getId());
+        transactionService.deleteById(transaction.getId());
+        verify(transactionRepository, times(1)).deleteById(transaction.getId());
+    }
+
+    @Test
+    void findAll() {
+        when(transactionService.findAll()).thenReturn(transactions);
+        transactionService.findAll();
+        verify(transactionRepository, times(1)).findAll();
+    }
+}
